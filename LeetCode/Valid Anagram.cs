cb1f6293b4e01de4b9796b79311a2c3ab559f582
Mohamed Ibrahim Fathy Metwally Awad <166850784+mo1482@users.noreply.github.com>
2026-09-11 18:59:30 +0300

    Create Valid Anagram.cs

---
+++ LeetCode/Valid Anagram.cs
@@ -0,0 +1,28 @@
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        int[] counts = new int[26];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+
+        for (int i = 0; i < counts.Length; i++)
+        {
+            if (counts[i] != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
